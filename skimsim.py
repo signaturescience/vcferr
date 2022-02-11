39e78247ccb82294d6a818e3aa5ce64eba1c40e7
VP Nagraj <pnagraj@signaturescience.com>
2022-02-11 08:06:54 -0600

    adding switch to stream conditioned on 'None' status of output_vcf arg

--- skimsim.py
+++ skimsim.py
@@ -2,9 +2,14 @@
 
 import pysam,random,argparse
 
-def skimsim(sample, input_vcf, p_het_dropout,p_hom_dropout,p_het_dropin,p_hom_dropin):
+def skimsim(sample, output_vcf, input_vcf, p_het_dropout,p_hom_dropout,p_het_dropin,p_hom_dropin):
 	vcf_in = pysam.VariantFile(input_vcf)
-	vcf_out = pysam.VariantFile("-", 'w', header=vcf_in.header)
+	## switch to allow streaming if no vcf_out is specified
+	if output_vcf is None:
+		vcf_out = pysam.VariantFile("-", 'w', header=vcf_in.header)
+	else:
+		vcf_out = pysam.VariantFile(output_vcf, 'w', header=vcf_in.header)
+
 	recs = vcf_in.fetch()
 
 	homref = [0,0]
@@ -66,7 +71,7 @@ if __name__ == '__main__':
 
 	parser.add_argument("--sample", help="ID of sample in VCF file to be simulated", default = "")
 	parser.add_argument("--input_vcf", help="VCF file to simulate ex: example.vcf.gz", default = "")
-	parser.add_argument("--output_vcf", help="Output VCF file containing simulated genotypes ex: example.sim.vcf.gz", default = "")
+	parser.add_argument("--output_vcf", help="Output VCF file containing simulated genotypes ex: example.sim.vcf.gz", default = None)
 	parser.add_argument("--p_het_dropout", help="Probability of heterozygous dropout (0,1) to (0,0)", default = 0.1, type=float)
 	parser.add_argument("--p_hom_dropout", help="Probability of homozygous dropout (1,1) to (0,1)", default = 0, type=float)
 	parser.add_argument("--p_het_dropin", help="Probability of heterozygous dropin (0,0) to (0,1)", default = 0, type=float)
@@ -75,10 +80,11 @@ if __name__ == '__main__':
 	args = parser.parse_args()
 	sample = args.sample
 	input_vcf = args.input_vcf
+	output_vcf = args.output_vcf
 	p_het_dropout = args.p_het_dropout
 	p_hom_dropout = args.p_hom_dropout
 	p_het_dropin = args.p_het_dropin
 	p_hom_dropin = args.p_hom_dropin
 
-	skimsim(sample=sample, input_vcf=input_vcf, p_het_dropout=p_het_dropout, p_hom_dropout=p_hom_dropout, p_het_dropin=p_het_dropin, p_hom_dropin=p_hom_dropin)
+	skimsim(sample=sample, input_vcf=input_vcf, output_vcf=output_vcf, p_het_dropout=p_het_dropout, p_hom_dropout=p_hom_dropout, p_het_dropin=p_het_dropin, p_hom_dropin=p_hom_dropin)
 
