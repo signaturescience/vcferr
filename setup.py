b2e149f29899b69c958ec66a701ee93a66e2e79e
VP Nagraj <pnagraj@signaturescience.com>
2022-03-07 11:59:48 -0600

    updating setup.py

--- setup.py
+++ setup.py
@@ -5,18 +5,18 @@ HERE=pathlib.Path(__file__).resolve().parent
 README=(HERE/"README.md").read_text()
 
 setup(
-	name='vcferr',
-	version='1.0.0',
-	description='Probabilistic VCF genotype error simulation',
-	long_description=README,
-	long_description_content_type="text/markdown",
-	url='https://github.com/signaturescience/vcferr',
-	author='VP Nagraj',
-	install_requires=['pysam'],
-	author_email='',
-	entry_points='''
-        	[console_scripts]
-        	vcferr=vcferr.__main__:vcferr    
-		''',
-	packages=setuptools.find_packages(),
-	zip_safe=False)
+    name='vcferr',
+    version='1.0.0',
+    description='Probabilistic VCF genotype error simulation',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url='https://github.com/signaturescience/vcferr',
+    project_urls={
+        'Bug Reports': 'https://github.com/signaturescience/vcferr/issues'},
+    install_requires=['pysam','click'],
+    entry_points='''
+        [console_scripts]
+        vcferr=vcferr.__main__:vcferr    
+    ''',
+    packages=setuptools.find_packages(),
+    zip_safe=False)
